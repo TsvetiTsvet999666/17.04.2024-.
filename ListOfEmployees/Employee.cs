94786623dc52f2c67865e59c272a573a517bdcf8
TsvetiTsvet999666 <159997613+TsvetiTsvet999666@users.noreply.github.com>
2024-04-17 16:35:03 +0300

    List of Employees

---
+++ ListOfEmployees/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfEmployees
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Employee(string name, decimal salary, string position, string department, string email, int age)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Department = department;
+            Email = email;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            string email = string.IsNullOrEmpty(Email) ? "n/a" : Email;
+            string age = Age <= 0 ? "-1" : Age.ToString();
+            return $"{Name} {Salary:F2} {Position} {Department} {email} {age}";
+        }
+    }
+}
---
+++ ListOfEmployees/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ListOfEmployees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] employeeInfo = Console.ReadLine().Split();
+                string name = employeeInfo[0];
+                decimal salary = decimal.Parse(employeeInfo[1]);
+                string position = employeeInfo[2];
+                string department = employeeInfo[3];
+                string email = employeeInfo.Length > 4 ? employeeInfo[4] : null;
+                int age = employeeInfo.Length > 5 ? int.Parse(employeeInfo[5]) : -1;
+                Employee employee = new Employee(name, salary, position, department, email, age);
+                employees.Add(employee);
+            }
+            var highestSalaryDepartment = employees
+                .GroupBy(e => e.Department)
+                .Select(g => new { Department = g.Key, AverageSalary = g.Average(e => e.Salary) })
+                .OrderByDescending(g => g.AverageSalary)
+                .First()
+                .Department;
+            var employeesFromHighestSalaryDepartment = employees
+                .Where(e => e.Department == highestSalaryDepartment)
+                .OrderByDescending(e => e.Salary);
+            foreach (var employee in employeesFromHighestSalaryDepartment)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
